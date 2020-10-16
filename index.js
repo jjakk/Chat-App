31371d7e4c0db0fd314be3fa1ae1a5a57826c14c
Jak <jaksawesome10@gmail.com>
2020-10-16 07:41:11 -0400

    spell fix

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-var app = require('express')();
+const express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+let bodyParser = require('body-parser');
 var port = process.env.PORT || 8000;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
 /* Routing */
 
 app.get('/', function(req, res){
   res.render('index');
+});
+
+app.post('/goto', (req, res) => {
+  res.redirect('/' + req.body.location);
 });
 
 app.get('/:id', function(req, res){
