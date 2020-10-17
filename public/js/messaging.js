3a74ce64b72e85651c52a710a42c52ad7069013a
Jak <jaksawesome10@gmail.com>
2020-10-17 10:49:07 -0400

    styling and stuff

--- public/js/messaging.js
+++ public/js/messaging.js
@@ -1,10 +1,12 @@
 $(function () {
     var socket = io();
+
     $('form').submit(function(){
       socket.emit('chat message', window.location.pathname, $('#m').val());
       $('#m').val('');
       return false;
     });
+    
     socket.on('chat message', function(msg){
       $('#messages').append($('<li>').text(msg));
       window.scrollTo(0, document.body.scrollHeight);
